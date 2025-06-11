fa89bc3239fa0ae3c2dbc2cba53281fe272b3569
angelo <angelo.dibella2020@gmail.com>
2025-06-11 19:38:19 +0200

    [nvim] [hyprland] Mess with theme and add warning to NVidia config in hyprland.conf

--- nvim/.config/nvim/lua/plugins/catppuccin.lua
+++ nvim/.config/nvim/lua/plugins/catppuccin.lua
@@ -2,6 +2,21 @@ return {
 	"catppuccin/nvim",
 	name = "catppuccin",
 	priority = 1000,
+	opts = {
+		compile_path = vim.fn.stdpath("cache") .. "/catppuccin",
+		highlight_overrides = {
+			all = function(colors)
+				return {
+					CmpBorder = { fg = "#3e4145" },
+				}
+			end,
+			mocha = function(mocha)
+				return {
+					Comment = { fg = mocha.flamingo },
+				}
+			end,
+		},
+	},
 	config = function()
 		vim.cmd.colorscheme("catppuccin-mocha")
 	end,
