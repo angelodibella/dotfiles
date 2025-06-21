65b8c94d4afe1d01225bb0bfae02356e82908a02
Angelo Di Bella <angelo.dibella2020@gmail.com>
2025-06-21 18:56:56 +0200

    [nvim] Minor theme adjustment

--- nvim/.config/nvim/lua/plugins/catppuccin.lua
+++ nvim/.config/nvim/lua/plugins/catppuccin.lua
@@ -1,23 +1,17 @@
 return {
 	"catppuccin/nvim",
 	name = "catppuccin",
+	lazy = false,
 	priority = 1000,
 	opts = {
-		compile_path = vim.fn.stdpath("cache") .. "/catppuccin",
-		highlight_overrides = {
-			all = function(colors)
-				return {
-					CmpBorder = { fg = "#3e4145" },
-				}
-			end,
-			mocha = function(mocha)
-				return {
-					Comment = { fg = mocha.flamingo },
-				}
-			end,
+		compile = {
+			enabled = true,
+			path = vim.fn.stdpath("cache") .. "/catppuccin",
 		},
+
+		flavor = "mocha",
 	},
 	config = function()
-		vim.cmd.colorscheme("catppuccin-mocha")
+		vim.cmd.colorscheme("catppuccin")
 	end,
 }
