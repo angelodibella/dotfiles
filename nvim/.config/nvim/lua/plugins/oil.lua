679067e3a325ac623cf41f535c8279908fbc84c5
Angelo Di Bella <angelo.dibella2020@gmail.com>
2025-12-01 14:39:19 +0000

    [nvim] Customize oil

--- nvim/.config/nvim/lua/plugins/oil.lua
+++ nvim/.config/nvim/lua/plugins/oil.lua
@@ -1,3 +1,21 @@
+local permission_hlgroups = {
+	["-"] = "NonText",
+	["r"] = "DiagnosticSignWarn",
+	["w"] = "DiagnosticSignError",
+	["x"] = "DiagnosticSignOk",
+}
+
+function _G.get_oil_winbar()
+	local bufnr = vim.api.nvim_win_get_buf(vim.g.statusline_winid)
+	local dir = require("oil").get_current_dir(bufnr)
+	if dir then
+		return vim.fn.fnamemodify(dir, ":~")
+	else
+		-- If there is no current directory (e.g. over ssh), just show the buffer name
+		return vim.api.nvim_buf_get_name(0)
+	end
+end
+
 return {
 	"stevearc/oil.nvim",
 	---@module 'oil'
@@ -10,6 +28,26 @@ return {
 			default_file_explorer = true,
 			delete_to_trash = true,
 			skip_confirm_for_simple_edits = true,
+			columns = {
+				{
+					"permissions",
+					highlight = function(permission_str)
+						local hls = {}
+						for i = 1, #permission_str do
+							local char = permission_str:sub(i, i)
+							table.insert(hls, { permission_hlgroups[char], i - 1, i })
+						end
+						return hls
+					end,
+				},
+				{ "size", highlight = "Special" },
+				{
+					"icon",
+					default_file = icon_file,
+					directory = icon_dir,
+					add_padding = false,
+				},
+			},
 			view_options = {
 				show_hidden = true,
 				natural_order = true,
@@ -24,6 +62,7 @@ return {
 			},
 			win_options = {
 				wrap = true,
+				winbar = "%!v:lua.get_oil_winbar()",
 				winblend = 0,
 			},
 			keymaps = {
