79b442d75c9b3de7cd89685b4aede9ed8385f4e4
Angelo Di Bella <angelo.dibella2020@gmail.com>
2026-01-14 15:13:03 +0000

    [zathura] Add config

--- nvim/.config/nvim/lua/plugins/latex.lua
+++ nvim/.config/nvim/lua/plugins/latex.lua
@@ -4,8 +4,7 @@ return {
 	-- tag = "v2.15", -- uncomment to pin to a specific release
 	init = function()
 		-- VimTeX configuration goes here, e.g.
-		vim.g.vimtex_general_viewer = "sioyek"
-		vim.g.vimtex_general_viewer_options = "--reuse-instance"
+		vim.g.vimtex_general_viewer = "zathura"
 		vim.g.vimtex_fold_manual = 1
 	end,
 }
