3e5bfc83f0cfbf1f72807fb46c3fed25d2bb4d36
Angelo Di Bella <angelo.dibella2020@gmail.com>
2026-04-11 13:57:38 +0100

    chore: some updates

--- nvim/.config/nvim/lua/plugins/latex.lua
+++ nvim/.config/nvim/lua/plugins/latex.lua
@@ -4,7 +4,7 @@ return {
 	-- tag = "v2.15", -- uncomment to pin to a specific release
 	init = function()
 		-- VimTeX configuration goes here, e.g.
-		vim.g.vimtex_general_viewer = "zathura"
+		vim.g.vimtex_general_viewer = "zathura_simple"
 		vim.g.vimtex_fold_manual = 1
 	end,
 }
